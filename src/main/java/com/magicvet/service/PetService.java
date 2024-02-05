5cbb14982ff24b0c7ce00420b0d93a27e6c97e70
ochuprov <rolanddef@gmail.com>
2024-02-05 16:54:31 +0200

    REQ-07: use enum for dog size

--- src/main/java/com/magicvet/service/PetService.java
+++ src/main/java/com/magicvet/service/PetService.java
@@ -38,8 +38,9 @@ public class PetService {
         pet.setSex(Main.SCANNER.nextLine());
 
         if (type.equals(DOG_TYPE)) {
-            System.out.println("Size: (xs/s/m/l/xl)");
-            ((Dog) pet).setSize(Main.SCANNER.nextLine());
+            System.out.println("Size: (XS/S/M/L/XL)");
+            String size = Main.SCANNER.nextLine();
+            ((Dog) pet).setSize(Dog.Size.valueOf(size));
         }
 
         return pet;
